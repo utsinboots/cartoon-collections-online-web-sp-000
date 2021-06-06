a4f39cbbb3054832118e4d7776d6fe485e936896
Utshant Gurung <utshantgurung@gmail.com>
2021-06-06 12:22:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,5 @@ def find_the_cheese(choices)
   cheese_types = ["cheddar", "gouda", "camembert"]
   #choices.each {|choice| return choice if cheese_types.include? (choice)}
  # return nil
-  choices.detect {|choice| choice.include? (cheese)}
+  choices.detect {|choice| choice.include? (cheese_types)}
 end
